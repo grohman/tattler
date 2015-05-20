e1db71661ee06c8aca6b656eb261256b6dfdcba9
Daniel Podrabinek <danyagrohman@gmail.com>
2015-05-21 00:03:20 +0300

    updates

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
+var conf = require('config');
+var cluster = require('cluster');
+if(cluster.isWorker){
+  conf['app']['port']=parseInt(conf['app']['port']) - 1 + parseInt(cluster.worker.id);
+}
+
 var server = require('nodebootstrap-server');
 
 server.setup(function(runningApp) {
-  
-  // runningApp.use(require('express-session')({secret: CONF.app.cookie_secret, resave: false, saveUninitialized: false}));
-  
-  // Choose your favorite view engine(s)  
   runningApp.set('view engine', 'handlebars');
-  runningApp.engine('handlebars', require('hbs').__express);
+  runningApp.engine('handlebars', require('hbs').__express); 
 
-  //// you could use two view engines in parallel (if you are brave):  
-  // runningApp.set('view engine', 'j2');
-  // runningApp.engine('j2', require('swig').renderFile);
-  
-  var socketio = require('socket.io')(runningApp.http);
+  var sio = require('socket.io');
   var tattler = require('tattler');
-  tattler.socket(socketio);
-
-
-  runningApp.use('/tattler', tattler);
-  runningApp.use(require('routes')); // attach to root route
+  tattler.socket(sio(runningApp.http));
   
+  runningApp.use('/tattler', tattler);
+  runningApp.use(require('routes'));  
 });
