2d3102abcde6b4318d44042914037c5d52338bc9
Daniel Podrabinek <danyagrohman@gmail.com>
2016-06-07 14:04:09 +0300

    added clustering

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var conf = require('config');
-var cluster = require('cluster');
-if(cluster.isWorker){
-  conf['app']['port']=parseInt(conf['app']['port']) - 1 + parseInt(cluster.worker.id);
+var cpus = conf['app']['numCpus'];
+if(cpus > 1 && process.env.NODE_CLUSTERED == 1) {
+	var cluster = require('cluster');
+	if(cluster.isWorker){
+  		conf['app']['port']=parseInt(conf['app']['port']) - 1 + parseInt(cluster.worker.id);
+	} else {
+    	for (var i = 0; i < cpus; i++) {
+        	cluster.fork();
+    	}
+	}
 }
 
 var server = require('nodebootstrap-server');
