68aadb5ae904908ccf0442eb622dcb5153144926
Daniel Podrabinek <dpodrabinek@oktopost.com>
2016-10-17 14:11:35 +0300

    Swapped redisClient with socket.io-redis; Separated logic;

--- appConfig.js
+++ appConfig.js
@@ -1,32 +1,23 @@
 require('app-module-path').addPath(__dirname + '/lib');
 
+var conf = require('config');
+
 exports.setup = function(runningApp, callback) {
   // Nothing ever comes from "x-powered-by", but a security hole
   runningApp.disable("x-powered-by");
 
-  // Choose your favorite view engine(s)
   runningApp.set('view engine', 'handlebars');
   runningApp.engine('handlebars', require('hbs').__express);
 
-  //// you could use two view engines in parallel (if you are brave):
-  // runningApp.set('view engine', 'j2');
-  // runningApp.engine('j2', require('swig').renderFile);
-
-
-  //---- Mounting well-encapsulated application modules (so-called: "mini-apps")
-  //---- See: http://expressjs.com/guide/routing.html and http://vimeo.com/56166857
-  // runningApp.use('/hello', require('hello')); // attach to sub-route
-
   var sio = require('socket.io');
+  var sioRedis = require('socket.io-redis');
   var tattler = require('Tattler');
-  tattler.socket(sio(runningApp.http));
+
+  tattler.socket(sio(runningApp.http), sioRedis({host: conf.redis.host, port: conf.redis.port}));
 
   // API endpoint attached to root route:
   runningApp.use('/tattler', tattler); // attach to root route
 
-  // If you need websockets:
-  // var socketio = require('socket.io')(runningApp.http);
-  // require('fauxchatapp')(socketio);
 
   if(typeof callback === 'function') {
     callback(runningApp);
